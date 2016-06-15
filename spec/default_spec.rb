531e70be9aea5ee81e4c7b05a77b0bff0ee75523
Austin Heiman <atheimanksu@gmail.com>
2016-06-15 14:47:22 -0500

    rspec tests covering nodes loaded in chef zero server

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,18 +1,43 @@
 require 'spec_helper'
+require 'chef_zero/server'
+require 'ridley'
+# get rid of Celluloid::TaskFiber warnings
+Ridley::Logging.logger.level = Logger.const_get 'ERROR'
+require 'json'
+require 'yaml'
 
 describe BetterChefRundeck do
-  # it 'has a version number' do
-  #   expect(BetterChefRundeck::VERSION).not_to be nil
-  # end
+  before(:context) do
+    # start the chef server
+    @server = ChefZero::Server.new(port: 4000)
+    @server.start_background
 
-  # it 'does something useful' do
-  #   expect(false).to eq(true)
-  # end
-end
+    # create the chef server objects in the chef zero server
+    chef_config_file = File.join(File.dirname(__FILE__), 'chef-fixtures', 'knife.rb')
+    ridley = Ridley.from_chef_config(chef_config_file)
+    chef_objects_file = File.join(File.dirname(__FILE__), 'chef-fixtures', 'chef-objects.json')
+    chef_objects = JSON.parse(File.read(chef_objects_file))
+    chef_objects['environments'].each { |env| ridley.environment.create env }
+    chef_objects['nodes'].each { |node| ridley.node.create node }
+    puts ridley.node.all
+  end
 
-describe "My Sinatra Application" do
-  it '/ should respond ok' do
-    get '/'
-    expect(last_response).to be_ok
+  context 'with nodes loaded into chef server' do
+    it '/ should respond ok' do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('spec/chef-fixtures/knife.rb')
+    end
+
+    it '/*:* should return all nodes with default filter result' do
+      get '/*:*'
+      expect(last_response).to be_ok
+      nodes = YAML.load(last_response.body)
+      expect(nodes.keys).to eq(['bcr-node', 'node-1', 'node-2'])
+    end
+  end
+
+  after(:context) do
+    @server.stop
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,20 @@
 # require_relative '../lib/better_chef_rundeck'
 require 'rack/test'
 require 'rspec'
-require 'chef_zero/server'
-require 'ridley'
-require 'json'
-
-# get rid of Celluloid::TaskFiber warnings
-Ridley::Logging.logger.level = Logger.const_get 'ERROR'
+require_relative '../lib/better_chef_rundeck'
 
 ENV['RACK_ENV'] = 'test'
-
-require_relative '../lib/better_chef_rundeck'
 
 # configure the app settings (this is done similarly in config.ru)
 BetterChefRundeck.configure do
   {
-    cache_dir:  File.join(__FILE__, '..', 'cache'),
+    cache_dir:  File.join(File.dirname(__FILE__), '..', 'cache'),
     cache_time: 30,
-    chef_config: File.join(__FILE__, 'chef-fixtures', 'knife.rb'),
+    chef_config: File.join(File.dirname(__FILE__), 'chef-fixtures', 'knife.rb'),
   }.each do |k, v|
     BetterChefRundeck.set k, v
   end
 end
-
-# start the chef server
-server = ChefZero::Server.new(port: 4000)
-server.start_background
-
-# create the chef server objects in the chef zero server
-chef_config_file = File.join(File.dirname(__FILE__), 'chef-fixtures', 'knife.rb')
-ridley = Ridley.from_chef_config(chef_config_file)
-chef_objects_file = File.join(File.dirname(__FILE__), 'chef-fixtures', 'chef-objects.json')
-chef_objects = JSON.parse(File.read(chef_objects_file))
-chef_objects['environments'].each { |env| ridley.environment.create env }
-chef_objects['nodes'].each { |node| ridley.node.create node }
-
 
 module RSpecMixin
   include Rack::Test::Methods
